3a1ca2a824916ae086f21534865565f944ae7cd5
pola2555 <100557073+pola2555@users.noreply.github.com>
2023-12-19 21:46:57 +0200

    Update RSA.py

--- Encryptions/Dual-Key/RSA/RSA.py
+++ Encryptions/Dual-Key/RSA/RSA.py
@@ -37,12 +37,14 @@ class rsa_keygenerator:
         self.n = self.p * self.q
         return self.n
     
-    def calculate_Euler_Totient(self):
-        self.phyn = (self.p - 1) * (self.q - 1)
-
     def phy(self):
         self.phyn = (self.p - 1) * (self.q - 1)
         return self.phyn
+    
+    def gcd(self, a, b):
+        while b:
+            a, b = b, a % b
+        return a
     
     def e_picker(self):
         '''
@@ -51,21 +53,37 @@ class rsa_keygenerator:
         self.e = 65537  # Commonly used value for 'e'
         while True:
             self.e = random.randint(3, self.phyn - 1)
-            if math.gcd(self.e, self.phyn) == 1:
+            if self.gcd(self.e, self.phyn) == 1:
                 break
         return self.e
+
+    def mod_inverse(self, a, m):
+        """
+        Calculate the modular inverse of 'a' modulo 'm' using the Extended Euclidean Algorithm.
+        """
+        m0, x0, x1 = m, 0, 1
+        while a > 1:
+            q = a // m
+            m, a = a % m, m
+            x0, x1 = x1 - q * x0, x0
+        if a != 1:
+            return None  # Modular inverse does not exist
+        if x1 < 0:
+            x1 += m0
+        return x1
 
     def d_picker(self):
         """
         Calculate the decryption exponent 'd' using the Extended Euclidean Algorithm.
         """
-        self.d = pow(self.e, -1, self.phyn)
+        self.d = self.mod_inverse(self.e, self.phyn)
         return self.d
+
     
     def key_generation(self, bits):
         self.p, self.q = self.generate_large_primes(bits, 2)
         self.calculate_n()
-        self.calculate_Euler_Totient()
+        self.phy()
         self.e_picker()
         self.d_picker()
         self.Keys = Keys_storing([self.e, self.n], [self.d, self.n])
@@ -76,28 +94,36 @@ class rsa_enc_and_dec:
         self.me = sender
         self.other = receiver
 
+    def custom_pow(self, base, exponent, modulus):
+        result = 1
+        while exponent > 0:
+            if exponent % 2 == 1:
+                result = (result * base) % modulus
+            exponent = exponent // 2
+            base = (base * base) % modulus
+        return result
+
     def encrypt(self, plaintext = ""):
         """
         (Send) Encrypt the plaintext message using the public key (e, n).
         """
         # Convert plaintext to numeric representation
         numeric_representation = int.from_bytes(plaintext.encode(), byteorder='big')
-        #encryption
-        ciphertext = pow(numeric_representation, self.other.public[0], self.other.public[1])
-        #ciphertext = pow(ciphertext, self.me.private[0], self.me.private[1])
+        # Encryption
+        ciphertext = self.custom_pow(numeric_representation, self.other.public[0], self.other.public[1])
         return ciphertext
     
     def decrypt(self, ciphertext):
         """
         (Receive) Decrypt the ciphertext using the private key (d, n).
         """
-        # Perform decryption
-        #decrypted_numeric = pow(ciphertext, self.other.public[0], self.other.public[1])
-        decrypted_numeric = pow(ciphertext, self.me.private[0], self.me.private[1])
+        # decryption
+        decrypted_numeric = self.custom_pow(ciphertext, self.me.private[0], self.me.private[1])
 
         # Convert numeric representation back to plaintext
-        plaintext = decrypted_numeric.to_bytes((decrypted_numeric.bit_length() + 7) // 8, byteorder='big').decode()
+        plaintext = decrypted_numeric.to_bytes((decrypted_numeric.bit_length()+7) // 8, byteorder='big').decode()
         return plaintext
+
 
 #example usage
 if __name__ == "__main__":
@@ -140,6 +166,3 @@ if __name__ == "__main__":
     c = Alice_app.encrypt("yeah me too :(")
     p = Bob_app.decrypt(c)
     print(f"Encrypted Message (Alice to Bob):\n{c}\nDecrypted Message (Bob received):\n{p}")
-
-
-
